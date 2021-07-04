3c8fe48ba37411c125eacdbdf734c51446e3e70b
EH-GD-MOHIT21 <experimentallyf@gmail.com>
2021-07-04 21:31:14 +0530

    version2.1 updated

--- js/player.js
+++ js/player.js
@@ -1,7 +1,27 @@
-UserSongLists = [0] // keeps a track of visited songs
-userindex = 0 // keeps a track of max_index
+// variables declare here
 
-// fake music api
+
+music = document.querySelector('audio');
+lineoffset = -50
+songisplaying = false
+is_paused = false
+
+userSong = []
+userindex = -1
+
+// custom colors of song icon
+listColors = [
+    "#00b894",
+    "#0984e3",
+    "#e84393",
+    "#6c5ce7",
+    "#d63031",
+    "#be2edd",
+    "#6ab04c",
+    "#22a6b3",
+    "#82589F",
+    "#6D214F"
+]
 
 const MEDIA_ROOT = '/media';
 
@@ -29,373 +49,288 @@ for (elm in MEDIA_INFO) length_obj += 1;
 // ends here fake api
 
 
-// change the public html data based on api data
-function changeNames(songname, songartist) {
-    document.getElementById('stitle').textContent = songname;
-    document.getElementById('sname').textContent = songartist;
+// functions Declarations starts here
+
+
+function playsong() {
+    if (!songisplaying) {
+        PlayingSong = document.getElementById('mainaudio');
+        PlayingSong.play();
+
+        songisplaying = true;
+        changePlayIcon();
+    } else if (!is_paused) {
+
+        try {
+            PlayingSong.pause();
+            is_paused = true;
+            changePlayIcon();
+
+        } catch (err) {
+            console.log();
+        }
+    } else if (is_paused) {
+
+        try {
+            PlayingSong.play();
+            is_paused = false;
+            changePlayIcon();
+
+        } catch (err) {
+            console.log();
+        }
+
+    }
 }
 
 
+function changeSongDetails(songName, artist) {
+    document.getElementById('stitle').textContent = songName;
+    document.getElementById('sname').textContent = artist;
+}
 
-// next song code here
 
-document.getElementById('nxtbtn').addEventListener('click', function() {
-    try {
-        choosetheme();
-        random_value = Math.floor(Math.random() * length_obj);
-        music = MEDIA_INFO[random_value]["name"]
-        singer = MEDIA_INFO[random_value]["singer"]
-        path = MEDIA_INFO[random_value]["path"]
-
-        try {
-            audiosong.pause();
-        } catch (err) {
-            console.log();
-        }
-
-        updateuservisited(true, random_value);
-        songAddress = path;
-
-        try {
-            clearInterval(setid);
-        } catch (err) {
-            console.log();
-        }
-
-        setid = setInterval(xyz, 100);
-        document.getElementById('mainaudio').src = songAddress;
-
-        changeNames(music, singer);
-        playAudio();
-
+function changePlayIcon() {
+    if (songisplaying && !is_paused) {
         document.getElementById('playbtn').className = "fa fa-pause-circle-o playbtn";
-        mohit = -50;
-
-        document.getElementById('mainplayline').style.transform = "translate(-52%,-50%)";
-        ispaused = false;
-
-        try {
-            clearInterval(id);
-        } catch (err) {
-            console.log();
-        }
-
-        id = setInterval(lineincreasefunc, 1000);
-
-    } catch (err) {
-        console.log(err);
-    }
-
-});
-
-
-function xyz() {
-    try {
-        parseInt(document.getElementById("mainaudio").duration);
-        playAudio(notfunccall = false);
-        clearInterval(setid);
-    } catch (err) {
-        console.log();
-    }
-}
-
-// prev code song here
-// pending work
-function updateuservisited(increment = true, songid = 0) {
-    if (increment) {
-        UserSongLists = UserSongLists.concat(parseInt(songid));
-        userindex += 1;
-        return 1;
     } else {
-        return infolastsong();
-    }
-}
-
-function infolastsong() {
-    if (UserSongLists.length > 1) {
-        cursong = UserSongLists.pop();
-        return_this = UserSongLists[userindex - 1]
-        userindex -= 1;
-        return return_this;
-    } else {
-        return UserSongLists[userindex]
-    }
-}
-
-mohit = -50;
-stateisReady = true;
-ispaused = false;
-
-
-// custom colors of song icon
-listColors = [
-    "#00b894",
-    "#0984e3",
-    "#e84393",
-    "#6c5ce7",
-    "#d63031",
-    "#be2edd",
-    "#6ab04c",
-    "#22a6b3",
-    "#82589F",
-    "#6D214F"
-]
-
-
-// alert('Use key left to prev 10sec key right to next 10sec')
-
-
-while (true) {
-    songAddress = prompt("Please provide me a path for your mp3 file like C:/users/mohit.mp3 or type mohit and press ok for default song.")
-    if (songAddress == "mohit") {
-        // choose Random 
-        random_value = Math.floor(Math.random() * length_obj);
-        music = MEDIA_INFO[random_value]["name"]
-        singer = MEDIA_INFO[random_value]["singer"]
-        path = MEDIA_INFO[random_value]["path"]
-        songAddress = path;
-        changeNames(music, singer);
-        UserSongLists = [random_value]
-        break;
-    }
-    if (songAddress != null && songAddress != "") {
-        if (prompt("provided path is " + songAddress + " type yes for confirm?") == "yes") {
-            break;
-        }
-    }
-}
-
-
-document.getElementById('mainaudio').src = songAddress;
-
-document.getElementById('playbtn').addEventListener('click', function() {
-    if (this.className == "fa fa-play-circle-o playbtn" && !stateisReady) {
-        this.className = "fa fa-pause-circle-o playbtn";
-        try {
-            audiosong.play();
-            ispaused = false;
-            return
-        } catch (err) {
-            console.log();
-        }
-    } else if (!stateisReady) {
-        this.className = "fa fa-play-circle-o playbtn"
-        try {
-            audiosong.pause()
-            ispaused = true;
-            return
-        } catch (err) {
-            console.log();
-        }
-    }
-    if (stateisReady) {
-        playAudio()
-        this.className = "fa fa-pause-circle-o playbtn";
-        id = setInterval(lineincreasefunc, 1000);
-    } else {
-        alert("song is already playing");
-    }
-})
-
-function lineincreasefunc() {
-    if (mohit > 50) {
-        clearInterval(id);
-
-        stateisReady = true;
-        mohit = -50;
-
         document.getElementById('playbtn').className = "fa fa-play-circle-o playbtn";
-        document.getElementById('mainplayline').style.transform = "translate(-52%,-50%)";
-        // play random music code
-    } else if (!ispaused) {
-        document.getElementById("mainplayline").style.transform = "translate(" + mohit + "%" + " , -50" + "%)";
-        mohit += (100 / finalDuration);
-        updatecurrenttime();
-        // console.log(mohit)
     }
 }
 
 
 
-function playAudio(notfunccall = true) {
-    if (notfunccall) {
-        audiosong = new Audio(songAddress);
-        audiosong.play();
-    }
-    // loading time for tag
-    finalDuration = Math.floor(document.getElementById("mainaudio").duration);
-    document.getElementById('starttime').textContent = "00:00";
-
-    minutes = parseInt(finalDuration / 60);
-    seconds = parseInt(finalDuration - minutes * 60);
-
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-    document.getElementById('finalDur').textContent = minutes + ":" + seconds;
-    stateisReady = false;
-}
-
-function updatecurrenttime() {
-    fakemohit = mohit + 50; // copy of variable(shifting origin)
-    starttime = Math.floor(finalDuration * fakemohit / 100);
-    starttime_min = Math.floor(starttime / 60);
-    starttime_sec = Math.floor(starttime - starttime_min * 60);
-    if (starttime_min < 10) {
-        starttime_min = "0" + starttime_min;
-    }
-    if (starttime_sec < 10) {
-        starttime_sec = "0" + starttime_sec;
-    }
-    document.getElementById('starttime').textContent = starttime_min + ":" + starttime_sec;
+function chooseSong() {
+    value = Math.floor(Math.random() * length_obj);
+    updateuservisitedsong(value);
+    return MEDIA_INFO[value];
 }
 
 
-// prev skip
+function updateSongOnHTML(href) {
+    document.getElementById('mainaudio').src = href;
+}
 
-document.getElementById('prevbtn').addEventListener('click', function() {
+
+
+function updateline(currentTime, duration) {
+    if (isNaN(currentTime))
+        return
+
+    if (isNaN(duration))
+        return
+
+    lineoffset = (currentTime / duration) * 100 - 50;
+    document.getElementById("mainplayline").style.transform = "translate(" + lineoffset + "%" + " , -50" + "%)";
+
+}
+
+
+function updatecurrenttime(currentTime) {
+    validform = sectominsec(currentTime);
+    document.getElementById('starttime').textContent = validform;
+}
+
+
+function updateFinalTime(Time) {
+    validform = sectominsec(Time);
+    document.getElementById('finalDur').textContent = validform;
+}
+
+
+function playprevsong() {
+    stopthesong();
+    if (userSong.length > 1) {
+        cursong = userSong.pop();
+        return_this = userSong[userindex - 1];
+        userindex -= 1;
+        songid = return_this;
+
+    } else {
+        songid = userSong[userindex];
+    }
+    changeSongDetails(MEDIA_INFO[songid]["name"], MEDIA_INFO[songid]["singer"]);
+    updateSongOnHTML(MEDIA_INFO[songid]["path"]);
+
     choosetheme();
-    songid = updateuservisited(false);
+    playsong();
+}
 
+
+function stopthesong() {
     try {
-        audiosong.pause();
-    } catch (err) {
-        console.log(err);
-    }
-    music = MEDIA_INFO[songid]["name"]
-    singer = MEDIA_INFO[songid]["singer"]
-    path = MEDIA_INFO[songid]["path"]
-
-    songAddress = path
-
-    try {
-        clearInterval(setid)
-    } catch (err) {
-        console.log()
-    }
-
-    setid = setInterval(xyz, 100);
-    document.getElementById('mainaudio').src = songAddress;
-
-    changeNames(music, singer);
-    playAudio();
-
-    document.getElementById('playbtn').className = "fa fa-pause-circle-o playbtn";
-    mohit = -50;
-
-    document.getElementById('mainplayline').style.transform = "translate(-52%,-50%)";
-    ispaused = false;
-
-    try {
-        clearInterval(id);
+        PlayingSong.pause();
+        is_paused = false;
+        songisplaying = false;
+        changePlayIcon();
     } catch (err) {
         console.log();
     }
-    id = setInterval(lineincreasefunc, 1000);
-})
+}
 
-function prevMusic() {
-    try {
-        audiosong.currentTime -= 10.0;
-        if ((mohit - ((100 / finalDuration) * (10.0))) >= -50)
-            mohit -= (100 / finalDuration) * (10.0);
-        else {
-            mohit = -50;
-        }
-    } catch (err) {
-        console.log(err);
-    }
+function updateuservisitedsong(value) {
+    userSong = userSong.concat([parseInt(value)]);
+    userindex += 1;
+}
+
+
+function playnewsong() {
+    stopthesong();
+    obj = chooseSong();
+    changeSongDetails(obj["name"], obj["singer"]);
+    updateSongOnHTML(obj["path"]);
+    choosetheme();
+    playsong();
 }
 
 
 
-function nextMusic() {
-    try {
-        audiosong.currentTime += 10.0;
-        if ((mohit + ((100 / finalDuration) * (10.0))) <= 50)
-            mohit += (100 / finalDuration) * (10.0);
-        else {
-            mohit = 50;
-        }
-    } catch (err) {
-        console.log(err);
-    }
+function setRandomSongOnce() {
+    obj = chooseSong();
+    changeSongDetails(obj["name"], obj["singer"]);
+    updateSongOnHTML(obj["path"]);
+}
+
+
+// time converter
+
+function sectominsec(timeinsec) {
+    minutes = Math.floor(parseInt(timeinsec) / 60);
+    seconds = Math.floor(parseInt(timeinsec) % 60);
+
+    if (minutes < 10)
+        minutes = "0" + minutes;
+
+    if (seconds < 10)
+        seconds = "0" + seconds;
+
+    return (minutes + ":" + seconds);
 }
 
 
 
-// key handler events
 
-document.addEventListener('keydown', (event) => {
-    var Keyname = event.key;
-
-    if (Keyname == 'ArrowRight')
-        nextMusic();
-
-    else if (Keyname == 'ArrowLeft')
-        prevMusic();
-
-    else if (Keyname == ' ') {
-        btn = document.getElementById('playbtn')
-        if (btn.className == "fa fa-play-circle-o playbtn" && !stateisReady) {
-            btn.className = "fa fa-pause-circle-o playbtn";
-            try {
-                audiosong.play();
-                ispaused = false;
-
-                return
-            } catch (err) {
-                console.log();
-
-            }
-        } else if (!stateisReady) {
-            btn.className = "fa fa-play-circle-o playbtn"
-            try {
-                audiosong.pause()
-                ispaused = true;
-
-                return
-            } catch (err) {
-                console.log();
-
-            }
-        }
-        if (stateisReady) {
-            playAudio()
-            btn.className = "fa fa-pause-circle-o playbtn";
-            id = setInterval(lineincreasefunc, 1000);
-        } else {
-            alert("song is already playing");
-        }
-    }
-
-}, false);
-
-
-document.getElementById("nfwd").addEventListener('click', function() {
-    nextMusic();
-})
-
-
-document.getElementById("bfwd").addEventListener('click', function() {
-    prevMusic();
-})
-
-
-
-// Random ico GeneRator
-
-
+// theme Generator
 
 function choosetheme() {
     randomValue = Math.floor(Math.random() * 10);
-    color = listColors[randomValue]
+    color = listColors[randomValue];
     document.getElementById('imgarea').style.color = color;
 }
 
 
 
-choosetheme();
+function backsometime() {
+    try {
+        PlayingSong.currentTime -= 10.0;
+    } catch (err) {
+        console.log();
+    }
+}
+
+
+function forwardsometime() {
+    try {
+        PlayingSong.currentTime += 10.0;
+    } catch (err) {
+        console.log();
+    }
+}
+
+
+
+
+//  element listners
+
+document.getElementById('playbtn').addEventListener('click', playsong);
+
+document.getElementById('nxtbtn').addEventListener('click', playnewsong);
+
+document.getElementById('prevbtn').addEventListener('click', playprevsong);
+
+document.getElementById('bfwd').addEventListener('click', backsometime);
+
+document.getElementById('nfwd').addEventListener('click', forwardsometime);
+
+
+
+
+
+// bug probabity high
+document.getElementById('parentline').addEventListener('click', (event) => {
+    const { duration } = music;
+
+    progress = (event.offsetX / event.srcElement.clientWidth) * duration;
+
+    // console.log(event, event.offsetX, event.srcElement.clientWidth);
+
+    try {
+        PlayingSong.currentTime = progress;
+    } catch (err) {
+        console.log();
+    }
+
+});
+
+// ends here (skip bug)
+
+
+
+
+// key handler events here
+
+document.addEventListener('keydown', (event) => {
+    var Keyname = event.key;
+
+    if (Keyname == 'ArrowUp')
+        playnewsong();
+
+    else if (Keyname == 'ArrowDown')
+        playprevsong();
+
+    else if (Keyname == ' ')
+        playsong();
+
+    else if (Keyname == 'ArrowRight')
+        forwardsometime()
+
+    else if (Keyname == 'ArrowLeft')
+        backsometime()
+
+}, false);
+
+
+
+// music click listener
+
+music.addEventListener("timeupdate", (event) => {
+    const { currentTime, duration } = event.srcElement;
+
+    if (!isNaN(currentTime)) {
+        updatecurrenttime(currentTime);
+    }
+
+    if (!isNaN(duration)) {
+        updateFinalTime(duration);
+    }
+
+    if ((!isNaN(currentTime)) && (!isNaN(duration))) {
+        updateline(currentTime, duration);
+    }
+
+    if (!isNaN(currentTime) && currentTime == duration) {
+        document.getElementById('playbtn').className = "fa fa-play-circle-o playbtn";
+        document.getElementById('starttime').textContent = "0:00";
+        document.getElementById('mainplayline').style.transform = "translate(-52%,-50%)";
+    }
+
+});
+
+
+
+
+
+
+// functions call
+setRandomSongOnce()
+choosetheme()
